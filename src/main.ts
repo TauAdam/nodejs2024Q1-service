7b2f6110cb4b55bf18d3ffdc8bc739bfb24cd9a0
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-03-16 19:00:59 +0500

    feat: fix and use OpenAPI file in doc folder

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 
+const swaggerRoute = 'doc';
 const port = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = await readFile('doc/api.yaml', { encoding: 'utf-8' });
+  const openApiDocument = parse(file);
+  SwaggerModule.setup(swaggerRoute, app, openApiDocument);
+
   await app.listen(port, () => {
-    console.log(`Server is running on localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log(
+      `Swagger is running on http://localhost:${port}/${swaggerRoute}`,
+    );
   });
 }
 bootstrap();
