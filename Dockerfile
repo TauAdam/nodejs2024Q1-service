a6ed1c6e7ec6a2353fdb89475ef927dbe45008f4
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-03-25 13:45:34 +0500

    feat: complete Containerization, Docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM node:20-alpine
+FROM node:20-alpine AS base
 WORKDIR /app
+EXPOSE 4000
+
+FROM base AS dev
 COPY package.json package-lock.json ./
-RUN npm install
+COPY ./prisma ./prisma
+RUN npm ci && npx prisma generate && npm cache clean --force
 COPY . .
-CMD ["npm","run","start:dev"]
+CMD npm run start:dev
--- src/repository/repository.module.ts
+++ src/repository/repository.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { PrismaService } from 'src/repository/prisma.service';
 import { RepositoryService } from 'src/repository/repository.service';
 
 @Global()
 @Module({
-  providers: [RepositoryService],
-  exports: [RepositoryService],
+  providers: [RepositoryService, PrismaService],
+  exports: [RepositoryService, PrismaService],
 })
 export class RepositoryModule {}
